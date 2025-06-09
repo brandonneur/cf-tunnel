00ab4b1715039d0af61d8118c59a178e069f4e2b
brandonneur <56902607+brandonneur@users.noreply.github.com>
2025-06-09 17:22:18 -0400

    Update cf-warp-install.sh

--- cf-warp-install.sh
+++ cf-warp-install.sh
@@ -1,7 +1,11 @@
+# install prerequisites
 apt update && apt install gpg lsb-release
 
+# install cloudflare gpg
 curl https://pkg.cloudflareclient.com/pubkey.gpg | gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
 
+# install cloudflare repository
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
 
+# install cloudflare tunnel WARP agent
 apt-get update && apt-get install cloudflare-warp
